2dff54c641634b8cd5548a3e3b3b1add0c03a749
Jimenez, Jose Alberto <tmxjosealberto.jimenez@kof.com>
2026-02-11 19:35:39 -0600

    build out the states

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,31 +1,79 @@
 type TaskData = {
-    id : string;
+    id: string;
     title: string;
-    state : 'TASK_ARCHIVED' | 'TASK_INBOX' | 'TASK_PINNED';
+    state: 'TASK_ARCHIVED' | 'TASK_INBOX' | 'TASK_PINNED';
 }
 
 type TaskProps = {
-    task :TaskData;
-    onArchiveTask: (id :string) => void;
-    onPinTask: (id:string) => void ;
+    task: TaskData;
+    //*Event to change the task to archive
+    onArchiveTask: (id: string) => void;
+    //* Event to change the task to pionned*/
+    onPinTask: (id: string) => void;
 }
 
 export default function Task({
-    task : {id, title, state },
+    /**composition of the task*/
+    task: { id, title, state },
     onArchiveTask,
     onPinTask,
-}: TaskProps){
+}: TaskProps) {
     return (
-    <div className="list-item">
-        <label htmlFor={`title=${id}`} aria-label={title}>
-            <input
-            type="text"
-            value={title}
-            readOnly={true}
-            name="title"
-            id={`title=${id}`}/>
-        </label>
-        </div>
-    );
+        <>
 
+            <div className={`list-item ${state}`}>
+                <label
+                    htmlFor={`{archiveTask=${id}`}
+                    aria-label={`archiveTask-${id}`}
+                    className="checkbox">
+                    <input
+                        type="checkbox"
+                        disabled={true}
+                        name="checked"
+                        id={`archiveTask-${id}`}
+                        checked={state == "TASK_ARCHIVED"}
+                    />
+                    <span className="checkbox-custom"
+                        onClick={() => onArchiveTask(id)} />
+                </label>
+            </div>
+
+            <label htmlFor={`title${id}`} aria-label={title} className="title">
+                <input
+                    type="text"
+                    value={title}
+                    readOnly={true}
+                    name="title"
+                    id={`title-${id}`}
+                    placeholder="Input title" />
+            </label>
+
+            {state !== "TASK_ARCHIVED" && (
+        <button
+        className="pin-button"
+            onClick={() => onPinTask(id)}
+            id={`pinTask-${id}`}
+            key={`pinTask-${id}`}
+        >
+            <span className="{`ìcon-star`}" />
+        </button >
+    )
 }
+
+</>
+);
+}
+
+    // <div className="list-item">
+    //     <label htmlFor={`title=${id}`} aria-label={title}>
+    //         <input
+    //         type="text"
+    //         value={title}
+    //         readOnly={true}
+    //         name="title"
+    //         id={`title=${id}`}/>
+    //     </label>
+    //     </div>
+    //     </>
+    // );
+
